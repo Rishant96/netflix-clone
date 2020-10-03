29fa2e086ea7bde190f7f47ecb3d62883095156d
Rishant <rishant.dutt96@gmail.com>
2020-10-03 14:23:23 -0400

    Almost done with Opt-Form

---
+++ src/components/opt-form/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Container, Input, Button, Break, Text } from "./styles/opt-form";
+
+export default function OptForm({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+}
+
+OptForm.Input = ({ ...restProps }) => {
+  return <Input {...restProps} />;
+};
+
+OptForm.Button = ({ children, ...restProps }) => {
+  return (
+    <Button {...restProps}>
+      {children} <img src="/images/icons/chevron-right.png" alt="Try Now" />
+    </Button>
+  );
+};
+
+OptForm.Break = ({ ...restProps }) => {
+  return <Break {...restProps} />;
+};
+
+OptForm.Text = ({ children, ...restProps }) => {
+  return <Text {...restProps}>{children}</Text>;
+};
